7a7d9825972babee18a7188b07864608a6839a6e
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-13 09:01:10 +0000

    20210513

--- aej_gutachten/.ipynb_checkpoints/_manifest_-checkpoint.py
+++ aej_gutachten/.ipynb_checkpoints/_manifest_-checkpoint.py
@@ -2,7 +2,9 @@
     "name": "AEJ Gutachten",
     "depends": ["base","documents","mail","contacts","project"],
     "data": [
+        'security/ir.model.access.csv',
         "views/gutachten.xml",
+        
     ],
     "installable": True,
 }
