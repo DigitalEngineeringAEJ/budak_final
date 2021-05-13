92588572b08e0e77460af9f4aca62077526d5958
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-13 09:22:37 +0000

    20210513

--- aej_gutachten/_manifest_.py
+++ aej_gutachten/_manifest_.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     "name": "AEJ Gutachten",
+    
+    'category': 'Uncategorized',
+    'version': '0.1',
+    
     "depends": ["base","documents","mail","contacts","project"],
     "data": [
         'security/ir.model.access.csv',
