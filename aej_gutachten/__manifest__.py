2c62b91d9a78d33b222fff494db43698382df024
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-14 14:44:15 +0000

    20210512

--- aej_gutachten/__manifest__.py
+++ aej_gutachten/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         #'security/ir.model.access.csv',
-        'views/aej_view.xml'
+        #'views/aej_view.xml'
     ],
     # only loaded in demonstration mode
     #'demo': [
