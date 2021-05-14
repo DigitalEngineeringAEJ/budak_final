b53937db965ae8e82f8bbe765a2eaaa2f2c63534
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-14 12:00:25 +0000

    20210512

--- aej_gutachten/__manifest__.py
+++ aej_gutachten/__manifest__.py
@@ -20,14 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
 
     # always loaded
     'data': [
         #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        #'views/aej_view.xml',
+        'views/aej_view.xml',
     ],
     # only loaded in demonstration mode
     #'demo': [
