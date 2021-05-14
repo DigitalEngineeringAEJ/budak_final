cd0debafcbe93db45b42cc72ac3fcc9d1f150cda
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-14 15:13:59 +0000

    20210512

--- aej_gutachten/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ aej_gutachten/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail','contacts','project','portal','web','digest'],
+    'depends': ['base','mail','contacts','project','portal','web','digest','documents','attachment_indexation'],
 
     # always loaded
     'data': [
