2a93d04625430e764f6b591db44a199499d5b7dd
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-15 08:21:19 +0000

    20210512

--- aej_gutachten/models/.ipynb_checkpoints/test-checkpoint.py
+++ aej_gutachten/models/.ipynb_checkpoints/test-checkpoint.py
@@ -9,10 +9,10 @@ class aej_gutachten(models.Model):
      _name = 'aej_gutachten.aej_gutachten'
      _description = 'aej_gutachten.aej_gutachten'
      _inherit = 'project.task'
-#    stage_id = fields.Many2one('project.task.type', 'stageXXX')
-#    currentDay = datetime.now().day
-#    currentMonth = datetime.now().month 
-#    currentYear = datetime.now().year
+    stage_id = fields.Many2one('project.task.type', 'stageXXX')
+    currentDay = datetime.now().day
+    currentMonth = datetime.now().month 
+    currentYear = datetime.now().year
 
     #Suchfunktion für Ordnerstruktur 
 #    wortliste = [str(currentYear),str(currentMonth)]
