4e36909becb86c1e934c104928a2db38944fc006
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-15 07:38:58 +0000

    20210512

--- aej_gutachten/__manifest__.py
+++ aej_gutachten/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail','contacts','project','portal','web','digest','documents','attachment_indexation'],
+    'depends': ['base','mail','contacts','project','portal','web','digest','documents.document','attachment_indexation'],
 
     # always loaded
     'data': [
