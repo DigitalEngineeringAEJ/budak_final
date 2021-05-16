e169b212cea3f334ae136343c461efd2ef6018f3
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-05-16 10:43:06 +0000

    20210516

--- my_module/models/aej_document.py
+++ my_module/models/aej_document.py
@@ -12,6 +12,86 @@ class aej_document(models.Model):
     currentMonth = datetime.now().month 
     currentYear = datetime.now().year
 
+    customer_name = fields.Char()
+    license_plate = fields.Char()
+    date_off_view = fields.Datetime()
+    place_offview = fields.Char()
+    
+    agent = fields.Char()
+    report_number = fields.Char()
+    day_offcrash = fields.Datetime()
+    place_offcrash = fields.Char()
+    
+    course_park = fields.Boolean()
+    park_out = fields.Boolean()
+    course_change_lane = fields.Boolean()
+    rear_end_collision = fields.Boolean()
+    
+    vorfahrt = fields.Boolean()
+    ast_park = fields.Boolean()
+    ast_standing = fields.Boolean()
+
+    others = fields.Boolean()
+    other_text = fields.Text()
+    
+    client_fam_name = fields.Char()
+    client_first_name = fields.Char()
+    client_post_code = fields.Char()
+    client_local = fields.Char()
+    client_post_code = fields.Char()
+    client_e_mail = fields.Char()
+    client_phone = fields.Char()
+    
+    client_lawyer = fields.Boolean()
+    client_name_lawyer = fields.Char()
+    client_insurance = fields.Boolean()
+    name_insurance = fields.Char()
+    client_service_book = fields.Boolean()
+    dekra = fields.Selection(selection=[('Dekra', 'Dekra'), ('PLZ Fahrzeugschein', 'PLZ Fahrzeugschein')])
+    
+    policy_holder_fam_name = fields.Char()
+    policy_holder_first_name = fields.Char()
+    policy_holder_post_code = fields.Char()
+    policy_holder_local = fields.Char()
+    policy_holder_streat = fields.Char()
+    
+    policy_name = fields.Char()
+    policy_number = fields.Char()
+    
+    selection_dismantle = fields.Selection(selection=[('unzerlegt', 'unzerlegt'), ('teilzerlegt', 'teilzerlegt')])
+    selection_driveable = fields.Selection(selection=[('fahrbereit', 'fahrbereit'), ('nicht fahrbereit', 'nicht fahrbereit'), ('bedingt fahrfähig bis zur Werkstatt', 'bedingt fahrfähig bis zur Werkstatt')])
+    selection_driving_safety = fields.Selection(selection=[('verkehrsicher', 'verkehrsicher'), ('nicht verkehrsicher', 'nicht verkehrsicher')])
+    
+    inspection_note = fields.Text()
+
+    selection_good = fields.Selection(selection=[('sehr gut', 'sehr gut'), ('gut', 'gut')])
+    selection_average = fields.Selection(selection=[('durchschnittlich', 'durchschnittlich'),('überdurchschnittlich', 'überdurchschnittlich')])
+    sehr_gepflegt = fields.Selection(selection=[('gepflegt', 'gepflegt'), ('sehr gepflegt', 'sehr gepflegt')])
+    selection_age_performance = fields.Selection(selection=[('Alter und Laufl. entsprechend', 'Alter und Laufl. entsprechend')])
+    
+    selection_car_good = fields.Selection(selection=[('sehr gut', ' sehr gut'), ('gut', 'gut')])
+    selection_car_average = fields.Selection(selection=[('überdurchschnittlich', 'überdurchschnittlich'), ('durchschnittlich', 'durchschnittlich')])
+    selection_car_condition = fields.Selection(selection=[('sehr gepflegt', 'sehr gepflegt'), ('gepflegt', 'gepflegt')])
+    selection_car_usable_dirty = fields.Selection(selection=[('Fahrzeug einsatzbereit verschmutzt','Fahrzeug einsatzbereit verschmutzt')])
+    
+    front_dimension = fields.Char()
+    front_index = fields.Char()
+    front_manufacturer = fields.Char()
+    front_wheel_type = fields.Selection(selection=[('Sommer', 'Sommer'), ('Winter', 'Winter')])
+    front_tread_depth = fields.Float()
+    
+    back_dimension = fields.Char()
+    back_index = fields.Char()
+    back_manufacturer = fields.Char()
+    back_wheel_type = fields.Selection(selection=[('Sommer', 'Sommer'), ('Winter', 'Winter')])
+    back_tread_depth = fields.Float()
+    
+    pre_damage = fields.Text()
+    
+    unrepaired_damage = fields.Text()
+    
+    signature = fields.Binary()
+    
     #Suchfunktion für Ordnerstruktur 
     wortliste = [str(currentYear),str(currentMonth)]
     trennzeichen = ' / '
