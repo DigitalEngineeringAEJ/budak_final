b22028f8b97b15829e7a9a127fcd2bbcedfcba84
wejdene <wberrich@hadooc.com>
2021-05-19 09:55:53 +0100

    [FIX] Review and fix module budak

--- budak/__manifest__.py
+++ budak/__manifest__.py
@@ -1,24 +1,15 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Budak",
-
-    'summary': """
+    "name": "Budak",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
+    "author": "Hadooc",
+    "website": "http://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "14.0.0.0.1",
+    "depends": ["documents_project"],
+    "data": ["report/schadenaufnahme.xml",
+             "views/project_task_views.xml"
 
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
-    'depends': ['documents_project'],
-
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-    ],
-
+             ],
 }
