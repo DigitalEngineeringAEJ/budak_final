273ec65c67e5f910d7778f6d97fc199b165146f2
wejdene <wberrich@hadooc.com>
2021-05-19 19:39:12 +0100

    [ADD] Add Schadenaufnahme report

--- budak/__manifest__.py
+++ budak/__manifest__.py
@@ -1,15 +1,10 @@
 {
     "name": "Budak",
-    "summary": """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
     "author": "Hadooc",
     "website": "http://www.yourcompany.com",
     "category": "Uncategorized",
     "version": "14.0.0.0.1",
     "depends": ["documents_project"],
-    "data": ["report/schadenaufnahme.xml",
-             "views/project_task_views.xml"
-
-             ],
+    "data": [
+        "views/project_task_views.xml", "report/schadenaufnahme.xml"],
 }
